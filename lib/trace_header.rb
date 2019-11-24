942569f7ce59d377073fb8fc80b396acff9814a6
Misaki Shioi <shioi.mm@gmail.com>
2019-11-24 09:27:13 +0900

    Take TraceHeader out of Rack module

--- lib/trace_header.rb
+++ lib/trace_header.rb
@@ -3,50 +3,48 @@ require 'active_support/core_ext'
 require_relative 'trace_header/description'
 require_relative 'trace_header/result'
 
-module Rack
-  class TraceHeader
-    include Description
+class TraceHeader
+  include Description
 
-    def initialize(app)
-      @app       = app
-      @outputs   = []
-      @fixed_app = nil
-    end
-
-    def call(env)
-      tracer.enable { @app.call(env) }
-      display(result)
-      @fixed_app
-    end
-
-    private
-      using Module.new {
-        refine TracePoint do
-          def called_rack_middleware?
-            method_id.eql?(:call) && binding.local_variable_defined?(:env)
-          end
-        end
-      }
-
-      def tracer
-        TracePoint.new(:call, :return) do |tp|
-          if tp.called_rack_middleware?
-            if tp.event.eql?(:call) \
-              && !@outputs.find { |input| input[:middleware].eql? tp.defined_class }
-              @outputs << { middleware: tp.defined_class,
-                            app: tp.self.deep_dup,
-                            env: tp.binding.local_variable_get(:env).deep_dup }
-            end
-
-            if tp.event.eql?(:return)
-              @fixed_app = tp.return_value
-            end
-          end
-        end
-      end
-
-      def result
-        Rack::TraceHeader::Result.new(@app, @outputs)
-      end
+  def initialize(app)
+    @app       = app
+    @outputs   = []
+    @fixed_app = nil
   end
+
+  def call(env)
+    tracer.enable { @app.call(env) }
+    display(result)
+    @fixed_app
+  end
+
+  private
+    using Module.new {
+      refine TracePoint do
+        def called_rack_middleware?
+          method_id.eql?(:call) && binding.local_variable_defined?(:env)
+        end
+      end
+    }
+
+    def tracer
+      TracePoint.new(:call, :return) do |tp|
+        if tp.called_rack_middleware?
+          if tp.event.eql?(:call) \
+            && !@outputs.find { |input| input[:middleware].eql? tp.defined_class }
+            @outputs << { middleware: tp.defined_class,
+                          app: tp.self.deep_dup,
+                          env: tp.binding.local_variable_get(:env).deep_dup }
+          end
+
+          if tp.event.eql?(:return)
+            @fixed_app = tp.return_value
+          end
+        end
+      end
+    end
+
+    def result
+      TraceHeader::Result.new(@app, @outputs)
+    end
 end
--- lib/trace_header/result.rb
+++ lib/trace_header/result.rb
@@ -1,51 +1,49 @@
-module Rack
-  class TraceHeader
-    class Result
-      attr_reader :target_app
+class TraceHeader
+  class Result
+    attr_reader :target_app
 
-      def initialize(target_app, outputs)
-        @target_app  = target_app
-        @target_hash = outputs.find { |output| output[:middleware].eql?(target_app.class) }
-        @inner_hash  = outputs[outputs.index(@target_hash) + 1]
-      end
-
-      def new_headers
-        headers(new_fields)
-      end
-
-      def changed_headers
-        headers(changed_fields)
-      end
-
-      private
-
-        def common_fields
-          target_header.keys & prev_header.keys
-        end
-
-        def new_fields
-          target_header.keys - common_fields
-        end
-
-        def changed_fields
-          common_fields.select { |field| target_header[field] != prev_header[field] }
-        end
-
-        def target_header
-          @target_header ||= rack_app(@target_hash)[1]
-        end
-
-        def prev_header
-          @prev_header ||= rack_app(@inner_hash)[1]
-        end
-
-        def rack_app(hash)
-          hash[:app].call(hash[:env])
-        end
-
-        def headers(fields)
-          fields.map { |field| { field => target_header[field].to_s } }
-        end
+    def initialize(target_app, outputs)
+      @target_app  = target_app
+      @target_hash = outputs.find { |output| output[:middleware].eql?(target_app.class) }
+      @inner_hash  = outputs[outputs.index(@target_hash) + 1]
     end
+
+    def new_headers
+      headers(new_fields)
+    end
+
+    def changed_headers
+      headers(changed_fields)
+    end
+
+    private
+
+      def common_fields
+        target_header.keys & prev_header.keys
+      end
+
+      def new_fields
+        target_header.keys - common_fields
+      end
+
+      def changed_fields
+        common_fields.select { |field| target_header[field] != prev_header[field] }
+      end
+
+      def target_header
+        @target_header ||= rack_app(@target_hash)[1]
+      end
+
+      def prev_header
+        @prev_header ||= rack_app(@inner_hash)[1]
+      end
+
+      def rack_app(hash)
+        hash[:app].call(hash[:env])
+      end
+
+      def headers(fields)
+        fields.map { |field| { field => target_header[field].to_s } }
+      end
   end
 end
